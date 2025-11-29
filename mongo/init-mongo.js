f3c47b8e8887099b34c255be0f291e7f12e2dadb
Alex Choux <choux.alex@gmail.com>
2025-11-29 17:40:53 +0100

    Init dockerised MongoDB with first simple collection
    Docker-compose v1
    Dockerfile for MongoDB v1
    Init mongoDB v1 with only a simple collection for files (one score from system + no speakers management)

---
+++ mongo/init-mongo.js
@@ -0,0 +1,66 @@
+db = db.getSiblingDB("pyannote_api_infos");
+
+db.createCollection("audio_file", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "Audio File Object Validation",
+            required: ["stockage_type", "filename", "gridfs_id"],
+            properties: {
+                human_score: {
+                    bsonType: "int",
+                    minimum: 0,
+                    maximum: 100,
+                    description: "Score given by the user after the diarization process. The higher is the score, the more the result is appreciated by the user."
+                },
+                system_score: {
+                    bsonType: "double",
+                    minimum: 0.0,
+                    maximum: 100.0,
+                    description: "Mean confidence score given by the system over the diarization process. The higher is the score, the more confident is the system."
+                },
+                diarization_result: {
+                    bsonType: "array",
+                    items: {
+                        bsonType: "object",
+                        required: ["start", "end", "speaker"],
+                        properties: {
+                            start: {
+                                bsonType: "double",
+                                minimum: 0.0
+                            },
+                            end: {
+                                bsonType: "double",
+                                minimum: 0.0
+                            },
+                            speaker: {bsonType: "string"}
+                        }
+                    },
+                    description: "Output of the system after processing diarization on the file."
+                },
+                file_path: {
+                    bsonType: "string",
+                    description: "Path of the file location in the storage system."
+                },
+                stockage_type: {
+                    bsonType: "string",
+                    enum: ["PyAnnote", "S3"],
+                    description: "File must be web stocked either using PyAnnote or Amazon S3."
+                },
+                filename: {
+                    bsonType: "string",
+                    description: "Local name of the file."
+                },
+                gridfs_id: {
+                    bsonType: "objectId",
+                    description: "ID created by MongoDB when the audio file have been registered."
+                }
+            }
+        }
+    }
+});
+
+db.audio_file.createIndex({"filename": 1}, {unique: true});
+
+//db.createCollection("pyannote_file");
+//db.createCollection("human_voiceprint");
